cea61bafc407e159925caf622a79ceca24a4568f
luisa gracia <luisagmazuca@gmail.com>
2022-02-28 13:21:03 -0700

    new hwk

---
+++ hwk5.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 28 12:30:47 2022
+
+@author: luisa
+"""
+
+kd_hydrophobicity= {
+    "I":4.5, "V":4.2, "L":3.8, "F":2.8,
+    "C":2.5, "M":1.9, "A":1.8, "G":-0.4,
+    "T":-0.7, "S":-0.8, "W":-0.9, "Y":-1.3,
+    "P":-1.6, "H":-3.2, "E":-3.5, "Q":-3.5,
+    "D":-3.5, "N":-3.5, "K":-3.9, "R":-4.5,
+    }
+
+def calc_hydrophobicity(sequence, start, w_len):
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
